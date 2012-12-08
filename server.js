cfa4d876cd3d880e1903acaff8bfeead287dafbd
Mathieu Laurent <mathieu.laurent@gmail.com>
2012-12-08 18:24:36 +0100

    Implement /population : Retrieve the population count (using http.request), wrap it to a json and send it

--- server.js
+++ server.js
@@ -1,11 +1,48 @@
 var express = require('express');
+var http = require('http');
 
 var app = express();
 
+// Set the folder with the static ressources (index.html, *.css, ...)
 app.use(express.static('public'));
 
-app.get('/population', function (req, res) {
-	res.send('Population');
+app.get('/population', function (request, response) {
+
+	// Get population on http://www.7billionandme.org/services/population.php 
+	var options = {
+	  host: 'www.7billionandme.org',
+	  port: 80,
+	  path: '/services/population.php'
+	};
+
+	// Get the content of the url with http.request (curl like)
+	var req = http.request(options, function(res) {
+		
+		var data;
+
+		res.setEncoding('utf8');
+		
+		res.on('data', function (chunk) {
+			//  retrieve the data from url
+			data = chunk;
+		});
+		
+		res.on('end',function() {
+			// Remove , from the data and cast to int
+			var count = parseInt(data.replace(/,/g,''),10);
+			var population = { 'timestamp' : new Date() , 'count' : count };
+			// Send json with response.json (set the correct mime-type header ...)
+			response.json(population);
+		});
+
+	});
+
+	req.on('error', function(e) {
+	  console.log('Problem with request: ' + e.message);
+	});
+
+	req.end();
+
 });
 
 app.listen(8000);
